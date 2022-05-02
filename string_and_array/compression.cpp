efa1438acbe25d71e0dd094d382489206b8bb4cf
Jabelic <hibiki990523@gmail.com>
2022-05-03 03:00:52 +0900

    ADD: compression

--- string_and_array/compression.cpp
+++ string_and_array/compression.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-// #include <set>
-// #include <string.h>
-// #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -13,6 +11,48 @@ using namespace std;
  * 文字列はアルファベットの大文字と小文字のみを想定してください。ヒント:#92′#110
 */
 
+string compress(string test_case){
+    long len = test_case.length();
+    int count = 0;
+    string val = "";
+    char last_char;
+    for(long i = 0; i < len; i++){
+        // 先頭
+        if(i==0){
+            val += test_case[i];
+            last_char =  test_case[i];
+        // 前の文字が今の文字と違うとき
+        }else if(test_case[i] != last_char){
+            if(count == 0){
+                val += test_case[i];
+                last_char =  test_case[i];
+            }else{
+                val+=to_string(count+1);
+                count = 0;
+                val += test_case[i];
+                last_char =  test_case[i];
+            }
+        // 文字が続いているとき
+        }else if(test_case[i] == last_char){
+            count++;
+            last_char =  test_case[i];
+            if(i == len-1)val+=to_string(count+1);
+        }
+    }
+    return val;
+}
+
 int main(){
+    // 辿って、やる
+    // 前と同じ文字だったらカウントをup
+    // 違ったらカウントを挿入してカウントを0にする
+    string test_case = "aabcddddddaaaa";
+    cout << (compress(test_case)=="a2bcd6a4") << endl;
+    test_case = "aabcddddddaaaaac";
+    cout << (compress(test_case)=="a2bcd6a5c") << endl;
+    test_case = "aaaa";
+    cout << (compress(test_case)=="a4") << endl;
+    test_case = "a";
+    cout << (compress(test_case)=="a") << endl;
     return 0;
 }
