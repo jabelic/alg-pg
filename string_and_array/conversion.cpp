f1777ec801412794d38791a75d02afe55bf2f057
Jabelic <hibiki990523@gmail.com>
2022-05-02 23:50:53 +0900

    FIX: insert

--- string_and_array/conversion.cpp
+++ string_and_array/conversion.cpp
@@ -12,20 +12,32 @@ bool isConvertable(string arg1, string arg2){
     long long arg1_len = arg1.length();
     long long arg2_len = arg2.length();
     long long count = 0;
+    // 文字列の長さが2以上違う場合はfalse
     if(abs((long long)arg1_len - (long long)arg2_len) > 1) return false;
     if(abs((long long)arg1_len - (long long)arg2_len) == 1){
+        // CASE: 少ない方に挿入すれば解決
         if((long long)arg1_len > (long long)arg2_len){
-            return (arg1.substr(0, arg1_len-1)==arg2 || arg1.substr(1, arg1_len)==arg2);
+            // return (arg1.substr(0, arg1_len-1)==arg2 || arg1.substr(1, arg1_len)==arg2);
+            for(long long i = 0; i < arg1_len; i++){
+                if(arg1.substr(0,i) + arg1.substr(i+1,-1) == arg2){
+                    return true;
+                }
+            }
         }else if((long long)arg1_len < (long long)arg2_len){
-            return (arg2.substr(0, arg2_len-1)==arg1 || arg2.substr(1, arg2_len)==arg1);
+            // return (arg2.substr(0, arg2_len-1)==arg1 || arg2.substr(1, arg2_len)==arg1);
+            for(long long i = 0; i < arg2_len; i++){
+                if(arg1 == arg2.substr(0,i) + arg2.substr(i+1,-1)){
+                    return true;
+                }
+            }
         }
     }
     else if(abs((long long)arg1_len - (long long)arg2.length()) == 0){
         if(arg2==arg1)return true;
         else{
+            // CASE: 1文字だけ違う場合
             for(long long i = 0; i < arg1_len; i++){
                if(arg2.substr(0,i)==arg1.substr(0,i) && arg2.substr(i+1,-1)==arg1.substr(i+1,-1)){
-                //    cout << arg1.substr(0,i) << " : " << arg1.substr(i+1, -1) << endl;
                    count++;
                };
             }
@@ -38,14 +50,20 @@ bool isConvertable(string arg1, string arg2){
 }
 
 int main(){
-    string h = "abcdefg";
-    cout << h.substr(0,-1) << endl;
-    cout << isConvertable("hoge", "hog") << endl;
-    cout << isConvertable("hoge", "hogo") << endl;
-    cout << isConvertable("hogehogehuga", "hogohogehuga") << endl;
-    cout << isConvertable("hogehogehuga123123", "hogohogehuga123123") << endl;
-    cout << isConvertable("hogehogehuga", "hogehogehuga1") << endl;
-    cout << isConvertable("hogehogehuga", "hogehogehuga12") << endl; // 0
-    cout << isConvertable("hogehogehuga", "hogehogeHUga") << endl; // 0
+    string success_case[7][2] = {{"hoge", "hog"},
+    {"hoge", "hogo"},
+    {"hoge", "hoe"},
+    {"hge", "hoge"},
+    {"hogehogehuga", "hogohogehuga"},
+    {"hogehogehuga123123", "hogohogehuga123123"},
+    {"hogehogehuga", "hogehogehuga1"},
+    };
+    for(int i = 0; i < 7 ; i++){
+        cout << (isConvertable(success_case[i][0], success_case[i][1])) << endl;
+    }
+    string fail_case[2][2] = {{"hogehogehuga", "hogehogehuga12"},{"hogehogehuga", "hogehogeHUga"}};
+    for(int i = 0; i < 2 ; i++){
+        cout << (isConvertable(fail_case[i][0], fail_case[i][1])==0) << endl;
+    }
     return 0;
 }
