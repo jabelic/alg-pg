3d5aec06d84f42da280b7aef256ed10840e566d3
Jabelic <hibiki990523@gmail.com>
2022-05-08 13:07:24 +0900

    ADD: split

--- linked_list/eliminate.cpp
+++ linked_list/eliminate.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 /**
  * @brief 間の要素を削除:単方向連結リストにおいて、間の要素(必ずしもちょうど中央というわけではなく、最初と最後の要素以外)で、
- * その要素 のみアクセス可能であるとします。その要素を削除するアルゴリズムを実装してください。
+ * その要素のみアクセス可能であるとします。その要素を削除するアルゴリズムを実装してください。
  例 入力:a―>b―>c―>d―>e―>fという連結リストのcが与えられます。 結果:何も返しませんが、リストはa‐>b―>d―>e―>fのように見えます。
-* 
+
+ 値が与えられるのではなく、該当のノードが与えられる。
+ -> 次のノードを現在のノードに上書きすればいい
+ *
  */
 
 forward_list<int> init_forward_list()
@@ -16,11 +19,22 @@ forward_list<int> init_forward_list()
     return list;
 }
 
-forward_list<int> eliminate(forward_list<int> arg, int target){
+void target_node(forward_list<int> arg, int _target,forward_list<int>::iterator *target)
+{
     for (forward_list<int>::iterator itr = arg.begin(); next(itr) != arg.end(); itr++)
     {
-        if(*next(itr) == target) arg.erase_after(itr);
+        if(*itr == _target){
+            cout << &itr<< endl;
+            // return &itr;
+            target = &itr;
+        }
     }
+}
+
+forward_list<int> eliminate(forward_list<int> arg, forward_list<int>::iterator target)
+{
+    forward_list<int>::iterator next_node = next(target);
+    next_node = target;
     return arg;
 }
 void print_forward_list(forward_list<int> arg)
@@ -33,11 +47,25 @@ void print_forward_list(forward_list<int> arg)
 }
 /** 重複した要素がないとする */
 /** 最初と最後は考えなくて良い */
-int main(){
-    int target = 22;
-    forward_list<int> list = init_forward_list();
+int main()
+{
+    int _target = 22;
+    // forward_list<int> list = init_forward_list();
+    forward_list<int>::iterator target;
+    forward_list<int> list{3, 2, 22, 3, 4, 1, 6, 54, 6, 7, 2, 10, 2};
     print_forward_list(list);
-    forward_list<int> eliminated_list = eliminate(list, target);
-    print_forward_list(eliminated_list);
+    for (forward_list<int>::iterator itr = list.begin(); next(itr) != list.end(); itr++)
+    {
+        if(*itr == _target){
+            cout << &itr<< endl;
+            target = itr;
+        }
+    }
+    // list = eliminate(list, target);
+    cout << &target<< endl;
+    target = next(target);
+    cout << &target<< endl;
+    print_forward_list(list);
+
     return 0;
 }
---
+++ linked_list/split.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+/** 
+ * リストの分割:ある数xが与えられたとき、連結リストの要素を並び替え、
+ * xより小さいものが前にくるようにするコードを書いてください。
+ * xがリストに含まれる場合、xの値はxより小さい要素の後にある必要があります(例 を参照してください)。
+ * 区切り要素のxは右半分のどこに現れてもかまいません。左半分と右半分のちょうど間にある必要はないということです。
+例
+    入力:3->5‐>8->5->10->2->1 [区切り要素 =5]
+    出力:3->1->2->10->5->5->8 ヒント:#3′ #24
+
+    どういうこと？
+ */
+
+list<int> init_list()
+{
+    list<int> list{3, 2, 2, 3, 4, 1, 3,5,6, 54, 6, 7, 2, 2, 2};
+    return list;
+}
+list<int> swap_order(list<int> list, int split){
+    for(auto itr = list.begin();itr!=list.end(); itr++){
+        if(*itr < split){
+            list.erase(itr);
+            list.push_front(*itr);
+        }
+    }
+    return list;
+}
+void print_list(list<int> arg)
+{
+    for (auto itr = arg.begin(); itr != arg.end(); itr++)
+    {
+        cout << *itr << ", ";
+    }
+    cout << endl;
+}
+int main(){
+    std::list<int> list = init_list();
+    std::list<int> swapped = swap_order(list, 5);
+    print_list(swapped);
+    return 0;
+}
